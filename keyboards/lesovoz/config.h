1d2411046aec468bec787bf855d0db70abddbc62
zackforbing <zforbing@vertafore.com>
2023-09-05 14:16:46 -0600

    created info.json from _33, increase combos, add window space to layout, enable nkro, beginning to update

--- keyboards/lesovoz/config.h
+++ keyboards/lesovoz/config.h
@@ -22,8 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0x5351
 #define PRODUCT_ID      0x4C53
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    squashkb
-#define PRODUCT         LESOVOZ
+#define MANUFACTURER    Squashkb
+#define PRODUCT         Lesovoz
 /* key matrix size */
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 10
