b2cdd2152e50e509aea54da6c355956049a4b9f9
zackforbing <zackforbing@gmail.com>
2023-11-19 21:38:13 -0700

    add qmk features to info.json

--- keyboards/treadstone32_c/rules.mk
+++ keyboards/treadstone32_c/rules.mk
@@ -14,12 +14,12 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
-NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+# BOOTMAGIC_ENABLE = yes       # Virtual DIP switch configuration
+# MOUSEKEY_ENABLE = yes       # Mouse keys
+# EXTRAKEY_ENABLE = yes       # Audio control and System control
+# CONSOLE_ENABLE = no         # Console for debug
+# COMMAND_ENABLE = no        # Commands for debug and configuration
+# NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
