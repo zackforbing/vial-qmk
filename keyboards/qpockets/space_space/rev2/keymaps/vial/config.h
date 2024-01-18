d3507cda71fc72dd5ba143bd6a6e67adbe0b3301
zackforbing <zackforbing@gmail.com>
2024-01-18 13:48:57 -0700

    add space space wip

---
+++ keyboards/qpockets/space_space/rev2/keymaps/vial/config.h
@@ -0,0 +1,22 @@
+/* Copyright 2021 qpockets
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x00, 0x41, 0xFF, 0xD2, 0xB3, 0xE6, 0xAA, 0x9B}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 2, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 10 }
