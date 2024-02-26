b613fa742eda3af565353415afac1272b3ae4eff
zackforbing <zackforbing@gmail.com>
2024-02-26 11:59:56 -0700

    add salisbury files

---
+++ keyboards/salisbury/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x9D, 0x05, 0xCA, 0x4E, 0x10, 0x3F, 0x1E, 0x2C}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
