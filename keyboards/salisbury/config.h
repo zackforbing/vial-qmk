2b2b4527b69b84dbd35e008f9282d2da108e199f
zackforbing <zackforbing@gmail.com>
2024-02-26 12:02:16 -0700

    add salisbury files

---
+++ keyboards/salisbury/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/* key matrix size */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 10
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { D7, B4, E6, B5 }
+#define MATRIX_COL_PINS { F5, D1, D0, D4, C6, B2, B3, B1, F7, F6 }
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION ROW2COL
