001ec1f0afd774a4bf0f1e34ebea00c41ddef6da
zackforbing <zackforbing@gmail.com>
2024-03-11 08:44:41 -0600

    added alieninvader, info.json changes

---
+++ keyboards/alieninvader/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x78, 0xB2, 0x36, 0xA3, 0x91, 0xCF, 0x5E, 0xB7}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
+
+#define VIAL_TAPDANCE_ENTRIES 16
+#define VIAL_COMBO_ENTRIES 32
+#define VIAL_KEY_OVERRIDE_ENTRIES 32
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
