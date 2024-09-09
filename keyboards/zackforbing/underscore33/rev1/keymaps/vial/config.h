5a28b245289b270c9f8d3c537525dd89a5cc69f3
Zack Forbing <zforbing@vertafore.com>
2024-09-09 12:55:02 -0400

    move to my folder

---
+++ keyboards/zackforbing/underscore33/rev1/keymaps/vial/config.h
@@ -0,0 +1,25 @@
+/* Copyright 2020 tominabox1
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+
+#define VIAL_KEYBOARD_UID {0x58, 0x02, 0x92, 0x4C, 0xD1, 0x2A, 0x71, 0xC6}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
+
+/* Combos */
+#define VIAL_COMBO_ENTRIES 32
