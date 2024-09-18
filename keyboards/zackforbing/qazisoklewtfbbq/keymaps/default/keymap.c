7e131a2d92f1b2fe0561ed75cde987a59f60f487
Zack Forbing <zforbing@vertafore.com>
2024-09-18 18:03:15 -0400

    complete bbq firmware

--- keyboards/zackforbing/qazisoklewtfbbq/keymaps/default/keymap.c
+++ keyboards/zackforbing/qazisoklewtfbbq/keymaps/default/keymap.c
@@ -2,11 +2,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-
        KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
        KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN,
        LSFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_MUTE,
-       KC_LCTL, KC_LGUI, LT(1,KC_BSPC),     LT(2,KC_SPC), KC_ENT, LT(3,KC_RALT)
+       KC_LGUI, LT(1,KC_SPC),     LT(2,KC_BSPC), KC_DEL
     )
 };
 
