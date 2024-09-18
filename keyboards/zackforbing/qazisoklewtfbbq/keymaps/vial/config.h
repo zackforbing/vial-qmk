8e28e9d8d76aa253b6869fc2551b9e88f1b5da4d
Zack Forbing <zforbing@vertafore.com>
2024-09-18 17:46:36 -0400

    add qazisoklewtfbbq

---
+++ keyboards/zackforbing/qazisoklewtfbbq/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xF9, 0x88, 0xD7, 0x99, 0x4D, 0xD1, 0xAD, 0x18}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
+
+#define VIAL_TAP_DANCE_ENTRIES 16
+#define VIAL_COMBO_ENTRIES 69
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 16
